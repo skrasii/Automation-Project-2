35161ea0377d2ccebc4faedb538eb9dda66b9193
skrasii <skrasii@gmail.com>
2023-07-04 01:08:44 +0300

    test commit

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -2,8 +2,8 @@ describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-      //System will already open issue creating modal in beforeEach block  
-      cy.visit(url + '/board?modal-issue-create=true');
+    //System will already open issue creating modal in beforeEach block  
+    cy.visit(url + '/board?modal-issue-create=true');
     });
   });
 
