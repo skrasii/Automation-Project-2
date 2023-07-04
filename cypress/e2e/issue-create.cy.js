fceae54c5d81e4a95c95c47d5f9ee1d58bbf2af0
skrasii <skrasii@gmail.com>
2023-07-04 14:08:18 +0300

    Assignment 2

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -56,7 +58,7 @@ describe('Issue create', () => {
     });
   });
 
-  it.only('Should create an issue with priority and validate it successfully', () => {
+  it('Should create an issue with priority and validate it successfully', () => {
 
     const issueData = {
       title: "Bug by SK",
@@ -84,11 +86,11 @@ describe('Issue create', () => {
       cy.get(`[data-testid="select-option:${issueData.type}"]`)
         .trigger('click');
 
-      //Select Pickle Rick from reporter dropdown
+      //Select user from reporter dropdown
       cy.get('[data-testid="select:reporterId"]').click();
       cy.get(`[data-testid="select-option:${issueData.reporter}"]`).click();
 
-      //Select Baby Yoda from assignee dropdown
+      //Select user from assignee dropdown
       cy.get('[data-testid="select:userIds"]').click();
       cy.get(`[data-testid="select-option:${issueData.assignee}"]`).click();
 
@@ -119,6 +121,70 @@ describe('Issue create', () => {
       //Assert that correct avatar and type icon are visible
       cy.get(`[data-testid="avatar:${issueData.reporter}"]`).should('be.visible');
       cy.get('[data-testid="icon:bug"]').should('be.visible');
+    });
+  });
+
+  it.only('Should create an issue with random data and validate it successfully', () => {
+
+    const issueData = {
+      title: faker.lorem.sentence(1),
+      type: "Task",
+      description: faker.lorem.sentence(),
+      assignee: "Baby Yoda",
+      reporter: "Baby Yoda",
+      priority: "Low"
+    }; 
+
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+
+
+      //Type value to description input field
+      cy.get('.ql-editor').type(issueData.description);
+
+      //Type value to title input field
+      //Order of filling in the fields is first description, then title on purpose
+      //Otherwise filling title first sometimes doesn't work due to web page implementation
+      cy.get('input[name="title"]').type(issueData.title);
+
+      // assert issue type 'task' selected
+      cy.get('[data-testid="icon:task"]').should('be.visible')
+  
+      //Select user from reporter dropdown
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get(`[data-testid="select-option:${issueData.reporter}"]`).click();
+
+      //Select user from assignee dropdown
+      cy.get('[data-testid="select:userIds"]').click();
+      cy.get(`[data-testid="select-option:${issueData.assignee}"]`).click();
+
+      cy.get('[data-testid="select:priority"]').click()
+      cy.get(`[data-testid="select-option:${issueData.priority}"]`).click()
+
+      // Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+
+    });
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+        .should('have.length', '5')
+        .find('p')
+        .contains(issueData.title);
+      //Assert that correct avatar and type icon are visible
+      cy.get(`[data-testid="avatar:${issueData.reporter}"]`).should('be.visible');
+      cy.get('[data-testid="icon:task"]').should('be.visible');
     });
   });
 
